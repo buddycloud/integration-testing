a81be2e6eb51842af8870a5fb3dd32739ef8f92e
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-10-28 16:22:23 +0000

    Checkout server if required, then reset and pull

--- resources/setup-buddycloud.sh
+++ resources/setup-buddycloud.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
-git clone https://github.com/buddycloud/buddycloud-server-java.git
+if [ ! -d "$DIRECTORY" ]; then
+  git clone https://github.com/buddycloud/buddycloud-server-java.git
+fi
 cd buddycloud-server-java
+git reset --hard
+git pull origin master
 mvn package
