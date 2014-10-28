5ad4e930676186bf3e216654ead0888515aff02e
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-10-28 08:52:23 +0000

    Fix database script

--- resources/setup-database.sh
+++ resources/setup-database.sh
@@ -1,19 +1,18 @@
 #! /bin/bash
 
-psql -c 'create database enterprise;' -U postgres
-psql -c 'create database voyager;' -U postgres
-psql -c 'create database borg;' -U postgres
-
-cd buddycloud-server-java
+#psql -c 'create database enterprise;' -U postgres
+#psql -c 'create database voyager;' -U postgres
+##psql -c 'create database borg;' -U postgres
 
 databases=( enterprise voyager borg)
-files=`find ./postgres -name "upgrade*.sql"`
+files=$(find ./buddycloud-server-java/postgres -name "upgrade*.sql")
 
 for i in "${databases[@]}"
 do
-    psql -U postgres $i < ./postgres/install.sql
-    for j in "${files[@]}"
+    psql -U postgres $i < ./buddycloud-server-java/postgres/install.sql
+    for j in $files
     do
+        echo "$i - $j"
         psql -U postgres $i < $j
     done
 done
