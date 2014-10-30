65f098f29ade778d76c3a79bf116cc0b6bf8c74d
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-10-30 14:20:26 +0000

    Handle XMPP logins and registration

--- helper.js
+++ helper.js
@@ -5,7 +5,7 @@ var Client = require('node-xmpp-client')
   , userMap = require('./utils/user-map')
 
 var connectedUsers = {}
-
+var lastConnection = null
 
 var XmppEventer = function() {}
 XmppEventer.prototype = new Event()
@@ -24,17 +24,44 @@ xmpp = new XmppEventer()
 manager = {
     socket: socket,
     client: xmpp,
+    tracking: {},
     trackId: function(id, callback) {
-        if (typeof id !== 'object') {
-            throw new Error('Stanza protection ID not added')
+        if (!id)
+            throw new Error(this.MISSING_STANZA_ID)
+        var jid
+        if (typeof id === 'object') {
+            if (!id.root().attrs.id)
+                throw new Error(this.MISSING_STANZA_ID)
+            jid = id.root().attrs.to
+            id = id.root().attrs.id
+            if (!jid){
+                jid = [
+                    this.getJidType('domain'),
+                    this.getJidType('bare')
+                ]
+            } else {
+                jid = [ jid ]
+            }
         }
-        this.callback = callback
+        if (!callback)
+            throw new Error(this.MISSING_CALLBACK)
+        if (typeof callback !== 'function')
+            throw new Error(this.INVALID_CALLBACK)
+        if (!this.client) {
+            return this.handleError(this.error.condition.NOT_CONNECTED)
+        }
+        this.tracking[id] = { callback: callback, jid: jid }
     },
     makeCallback: function(error, data) {
         this.callback(error, data)
     },
     setConnection: function(connection) {
         this.client = connection
+        var self = this
+        this.client.on('stanza', function(stanza) { self.catchTracked(stanza) })
+    },
+    getConnection: function() {
+        return this.client
     },
     getSocket: function() {
         return this.socket
@@ -46,14 +73,45 @@ manager = {
             error: function() {},
             warn: function() {}
         }
+    },
+    catchTracked: function(stanza) {
+        var id = stanza.root().attr('id')
+        if (!id || !this.tracking[id]) return false
+        if (this.tracking[id].jid &&
+            stanza.attr('from') &&
+            (-1 === this.tracking[id].jid.indexOf(stanza.attr('from')))) {
+            // Ignore stanza its an ID spoof!
+            return true
+        }
+        this.tracking[id].callback(stanza)
+        return true
     }
 }
 var buddycloud = new Buddycloud()
 buddycloud.init(manager)
 
-var getConnection = function(user, server, callback) {
+var getConnection = function(user, server, callback, registered) {
     var options = userMap.getDetails(user, server)
+    if (!registered) {
+        options.register = true
+    }
     var client = new Client(options)
+    log('Attempting to log in ', options)
+    client.on('online', function(data) {
+        log('User ' + user + '@' + server + ' logged in')
+        connectedUsers[user + '@' + server] = client
+        lastConnection = client
+        callback(null, client)
+    })
+    client.on('error', function(error) {
+        if (error.message === 'Registration error') {
+            log('User already registered, so logging in')
+            delete options.register
+            return getConnection(user, server, callback, true)
+        }
+        log('Error logging in', error)
+        callback(error)
+    })
     
 }
 
@@ -61,9 +119,14 @@ var setLastConnection = function(connection) {
     manager.setConnection(connection)
 }
 
+var getLastConnection = function() {
+    manager.setConnection(lastConnection)
+    return lastConnection
+}
+
 module.exports = {
     getConnection: getConnection,
     buddycloud: buddycloud,
     manager: manager,
-    setLastConnection: setLastConnection
+    getLastConnection: getLastConnection
 }
--- utils/user-map.js
+++ utils/user-map.js
@@ -1,20 +1,37 @@
+var extend = require('extend')
+
 var serverMap = {
-    'enterprise': 'enterprise.sf',
-    'voyager': 'voyager.sf',
-    'borg': 'borg.collective'
+    'enterprise': {
+        domain: 'enterprise.sf',
+        users: {
+            riker: 'i-am-number-one'
+        }
+    },
+    'voyager': {
+        domain: 'voyager.sf',
+        users: {}
+    },
+    'borg': {
+        domain: 'borg.collective',
+        users: {}
+    }
 }
 
-var getDetails = function(user, server) {
-    var domain = serverMap[server.toLowerCase()]
-    if (!domain) throw new Error('Unknown group (' + server + ')!')
+var getDetails = function(local, server) {
+    var local = local.toLowerCase()
+    var server = server.toLowerCase()
+    
+    var domain = serverMap[server]
+    if (!domain.domain) throw new Error('Unknown group (' + server + ')!')
+    var password = domain.users[local]
+    if (!password) throw new Error('Unknown user (' + local + '@' + server + ')!')
     return {
-        jid: user + '@' + domain,
+        jid: local + '@' + domain.domain,
+        password: password,
         host: process.env.XMPP_HOST || 'localhost',
         port: process.env.XMPP_PORT || 5222
     }
 }
-
-
 
 module.exports = {
     getDetails: getDetails
